b86b831a7822f8f43fb64baaa912a43e5fd09a9e
Joshua vayle <josh75337@gmail.com>
2018-08-28 16:11:34 -0400

    Add project files.

---
+++ ProjectAva/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectAva.Startup))]
+namespace ProjectAva
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
